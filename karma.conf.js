79e9cf618098596988291d2355ff12c5c2905d9e
d.bukhonova <d.bukhonova@xsolla.com>
2022-04-25 00:05:24 +0500

    find factorial2

--- karma.conf.js
+++ karma.conf.js
@@ -74,10 +74,6 @@ module.exports = function(config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity,
-    flags: [
-      '--disable-gpu',
-      '--no-sandbox'
-  ]
+    concurrency: Infinity
   });
 }
