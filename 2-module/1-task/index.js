1fa38e89aa55c4460aeb931884a34614df3b7daf
d.bukhonova <d.bukhonova@xsolla.com>
2022-04-26 23:54:40 +0500

    2-module

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,13 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries){
+    if (
+      typeof salaries[key] === "number" &&
+      Number.isFinite(salaries[key]) === true
+    ) {
+      sum += salaries[key];
+    }
+  }
+  return sum; 
+
 }
