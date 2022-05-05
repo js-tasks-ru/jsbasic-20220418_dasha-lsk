f64333711877f8bed7e4a943cab51e9560893936
d.bukhonova <d.bukhonova@xsolla.com>
2022-05-05 20:57:24 +0500

    3-module

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,7 @@
 function camelize(str) {
-  // ваш код...
+  let str2 = str.split('-');
+  for (var i = 1; i < str2.length; i++) {
+    str2[i] = str2[i].charAt(0).toUpperCase() + str2[i].slice(1);
+  }
+  return str2.join("");
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,9 @@
 function getMinMax(str) {
   // ваш код...
+  let arrayOfNumbersAndStrings = str.split(" ");
+  let numbersAndNans = arrayOfNumbersAndStrings.map(item => Number(item));
+  let numbers = numbersAndNans.filter(item => !isNaN(item));
+  let max = Math.max(...numbers);
+  let min = Math.min(...numbers);
+  return {min: min, max: max};
 }
